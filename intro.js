69ce5e5dd304aa9d2aef0f6d08d2d6ae23c1be3b
starpro23 <23starpro.32@gmail.com>
2025-11-09 01:46:37 +0300

    first commit

--- intro.js
+++ intro.js
@@ -1,9 +1,9 @@
 
 const prompt = require("prompt-sync")();
 
-const name = prompt("Enter your name");
-const age = prompt(" Enter your age");
-const technologies = prompt(" Enter your favourite programming languages");
+const name = prompt("Enter your name: ");
+const age = prompt(" Enter your age: ");
+const technologies = prompt(" Enter your favourite programming languages: ");
 
 const favoriteLanguages = technologies.split(',').map(lang=> lang.trim());
 
